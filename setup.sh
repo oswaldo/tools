aa6a93b5128be6476c94a91b6ce21cebc36bb84f
Oswaldo Dantas <oswaldodantas@gmail.com>
2023-10-07 15:28:17 +0200

    setup curl call adjustment

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export TERM=dumb
 if ! command -v scala-cli &> /dev/null
 then
     echo "Scala CLI is not installed, installing it now..."
-    curl -s https://github.com/VirtusLab/scala-cli/raw/main/scala-cli.sh | sh
+    curl -fsSL https://github.com/VirtusLab/scala-cli/raw/main/scala-cli.sh | bash -s --
 else
     echo "Scala CLI is installed, continuing..."
 fi
