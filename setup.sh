7884402eaf89d2095f615473914e997dcbe757b0
Oswaldo Dantas <oswaldodantas@gmail.com>
2023-10-07 15:16:59 +0200

    changed scala-cli installer url to the one recommended in the script itself

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export TERM=dumb
 if ! command -v scala-cli &> /dev/null
 then
     echo "Scala CLI is not installed, installing it now..."
-    curl -s https://raw.githubusercontent.com/VirtusLab/scala-cli/main/scala-cli.sh | sh
+    curl -s https://github.com/VirtusLab/scala-cli/raw/main/scala-cli.sh | sh
 else
     echo "Scala CLI is installed, continuing..."
 fi
