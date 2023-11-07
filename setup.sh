ca8b8c97bfe7a65566faa2c870f41a0937adf67a
Oswaldo Dantas <oswaldodantas@gmail.com>
2023-11-08 00:10:03 +0100

    added gnupg, minor fixes and formatting (#22)

--- setup.sh
+++ setup.sh
@@ -4,15 +4,13 @@
 #export TERM=dumb
 
 # Checks if scala-cli is installed. If it is, runs the finishSetup.sc script. If not, runs the script in the scala-cli.sh directly fetched from the scala-cli repository. The finishSetup script will take care of doing the propper scala-cli instalation afterwards 🤯
-if ! command -v scala-cli &> /dev/null
-then
+if ! command -v scala-cli &>/dev/null; then
     echo "Scala CLI is not installed, no problemo..."
     curl -fsSL https://github.com/VirtusLab/scala-cli/raw/main/scala-cli.sh | bash -s finishSetup.sc
 else
     echo "Scala CLI is installed, continuing..."
     ./finishSetup.sc
 fi
-
 
 # All the rest is done by the tools project using Scala CLI to run the finishSetup.sc script
 # Yes, it is that simple! :)
